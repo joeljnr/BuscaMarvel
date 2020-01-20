a7c5c3fde48c0837137eb7e1d54a52edac4b113b
Joel Junior <joel.asj2@gmail.com>
2020-01-20 16:35:55 -0300

    initial commit

---
+++ app/src/main/java/com/jnr/buscamarvel/utils/Utils.kt
@@ -0,0 +1,2 @@
+package com.jnr.buscamarvel.utils
+
---
+++ app/src/main/java/com/jnr/buscamarvel/views/HeroesActivity.kt
@@ -0,0 +1,13 @@
+package com.jnr.buscamarvel.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jnr.buscamarvel.R
+
+class HeroesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heroes)
+    }
+}
